1e08bac818b55bed59a4cd000cef80a80d73e979
mr_lipovsky <90620664+mrLipovsky@users.noreply.github.com>
2022-09-06 09:37:25 +0200

    Add files via upload

---
+++ starter/script.js
@@ -0,0 +1,50 @@
+let js = "amazing";
+if (js === "amazing") alert('Peter Lipo je boh');
+
+40 + 8 + 23 - 10
+console.log(40 + 8 + 23 - 10); 
+
+// values
+console.log('Jonas');
+console.log(23);
+
+let firstName = 'Jonas';
+console.log(firstName);
+
+// variables pisat takto - male a potom velke pismena 
+let firstNamePeter = 'Peter';
+
+//boolien 
+true;
+console.log(true);
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+// typeof je typ premennej boolien, string, number, undefined null...
+console.log(typeof true);
+console.log(typeof javascriptIsFun);
+console.log(typeof 23);
+console.log(typeof 'Jonas');
+
+// 12. change the variable bez let na zaciatku
+javascriptIsFun = 'YES!';
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1993;
+
+console.log(typeof null)
+
+// 13. let, const and var
+let age = 30;
+age = 31;
+
+// can not change variable with const - error
+const birthYear = 1993;
+birthYear = 1998;
+
+const job;
+
