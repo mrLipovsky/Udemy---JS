233c66269bb28cd230e00e77cca420aa9ee385c4
mr_lipovsky <90620664+mrLipovsky@users.noreply.github.com>
2022-11-08 12:33:01 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+//Operators
+
+const now = 2037
+const ageJonas = now - 1991;
+const ageSarah = now - 1991;
+console.log(ageJonas, ageSarah);
+
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3)
+
+const firstName = 'Peter';
+const lastName = 'Lipovsky';
+console.log(firstName + ' ' + lastName)
+
+let x = 10 + 5; //15
+x += 10; // x = x + 10
+x *= 10; // x = x * 10
+x++; // x = x + 1
+x-- // x = x - 1
+console.log(x);
+
+//Comparison Operators 
+console.log(ageJonas > ageSarah)
+console.log(ageJonas >= ageSarah)
